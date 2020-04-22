b8228a10763559bc834d5e9c49a39d905f29a683
sakura <sakura1@example.com>
2020-04-22 10:58:13 +0100

    create calculator.c

---
+++ calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(int argc, char *argv[]){
+	printf(argv[0]);
+	printf("\n");
+	float principal = atof(argv[1]);
+	float interest = atof(argv[2]); 
+	float years = atof(argv[3]); 
+	float answer = principal;
+	int i;
+	printf("%f\n", principal);
+	for(i = 0; i < years; i++){
+		answer = answer * (1 + interest/100);
+	}
+	printf("%f\n",answer);
+	return 0;
+}
+
+
