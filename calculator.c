9de91edff0418e8628a34a7c022f27bb0d302572
sakura <sakura1@example.com>
2020-05-02 21:33:11 +0100

    modify the output text

--- calculator.c
+++ calculator.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[]){
-	printf(argv[0]);
-	printf("\n");
 	float principal = atof(argv[1]);
 	float interest = atof(argv[2]); 
 	float years = atof(argv[3]); 
 	float answer = principal;
 	int i;
-	printf("%f\n", principal);
 	for(i = 0; i < years; i++){
 		answer = answer * (1 + interest/100);
 	}
-	printf("%f\n",answer);
+	printf("%f become %f.\n", principal, answer);
+	//anser = principal * ( 1.05 ^ years )
 	return 0;
 }
 
